65e972db5753c79ed93f8a130795bac7642b13cf
Dheerajreddy123 <107562188+Dheerajreddy123@users.noreply.github.com>
2022-06-15 20:08:04 +0530

    Create MultiplicationTable.java

---
+++ Lecture 4 : ConditionalsandLoops/MultiplicationTable.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+	
+	public static void main(String[] args) {
+        
+		int n;
+        Scanner s=new Scanner(System.in);
+        int i=1;
+        n=s.nextInt();
+        while(i<=10){
+         
+          
+            System.out.println(n*i);
+            
+            i=i+1;
+        }
+
+	}
+}
