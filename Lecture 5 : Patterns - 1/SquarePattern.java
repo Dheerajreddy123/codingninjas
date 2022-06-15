d1ae984b7e3900af2a7c47f79d782a5c2126379d
Dheerajreddy123 <107562188+Dheerajreddy123@users.noreply.github.com>
2022-06-15 20:12:26 +0530

    Create SquarePattern.java

---
+++ Lecture 5 : Patterns - 1/SquarePattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+
+
+	public static void main(String[] args) {
+        int i,j,N;
+        Scanner s=new Scanner(System.in);
+        N=s.nextInt();
+		for(i=1;i<=N;i++){
+          for(j=1;j<=N;j++){
+            System.out.print(N);
+        } 
+            System.out.print("\n");
+        }
+		
+	}
+
+}
